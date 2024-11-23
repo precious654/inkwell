14bead25b8ab72e42c84e2302449e97206c88f0e
precious654 <odukwechiemeka@gmail.com>
2024-11-23 14:49:01 +0100

    initialized project and handled authentication

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import {Poppins} from "next/font/google";
+import "../globals.css";
+import Nav from "@/app/(components)/Nav";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "600"],
+    display: "swap"
+})
+
+export const metadata: Metadata = {
+  title: "Inkwell",
+  description: "Read and share inspirational narratives",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={poppins.className}
+      >
+      <div className="container">
+          <Nav />
+          {children}
+      </div>
+      </body>
+    </html>
+  );
+}
